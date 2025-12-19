6755d561204012fc8b21bdd6a6a1545e3012c056
Nikos Kapatos <nikos@kapatos.com>
2025-12-19 13:58:32 +0100

    feat(neoweaver): add tree keybindings and commands for collection CRUD operations

--- clients/neoweaver/plugin/neoweaver.lua
+++ clients/neoweaver/plugin/neoweaver.lua
@@ -107,3 +107,111 @@ end, {
   end,
   desc = "Neoweaver collections explorer",
 })
+
+-- Collections commands
+vim.api.nvim_create_user_command("NeoweaverCollectionCreate", function(opts)
+  local collections = require("neoweaver._internal.collections")
+  
+  -- Parse args: name [parent_id]
+  local args = vim.split(opts.args, "%s+")
+  local name = args[1]
+  local parent_id = args[2] and tonumber(args[2]) or nil
+  
+  if not name or name == "" then
+    vim.notify("Usage: :NeoweaverCollectionCreate <name> [parent_id]", vim.log.levels.WARN)
+    return
+  end
+  
+  collections.create_collection(name, parent_id, function(collection, err)
+    if err then
+      vim.notify("Failed to create collection: " .. (err.message or vim.inspect(err)), vim.log.levels.ERROR)
+      return
+    end
+    vim.notify("Created collection: " .. collection.displayName, vim.log.levels.INFO)
+  end)
+end, { nargs = "+", desc = "Create new collection" })
+
+vim.api.nvim_create_user_command("NeoweaverCollectionRename", function(opts)
+  local collections = require("neoweaver._internal.collections")
+  
+  -- Parse args: id new_name
+  local args = vim.split(opts.args, "%s+", { trimempty = true })
+  local id = tonumber(args[1])
+  local new_name = table.concat(vim.list_slice(args, 2), " ")
+  
+  if not id or not new_name or new_name == "" then
+    vim.notify("Usage: :NeoweaverCollectionRename <id> <new_name>", vim.log.levels.WARN)
+    return
+  end
+  
+  collections.rename_collection(id, new_name, function(collection, err)
+    if err then
+      vim.notify("Failed to rename collection: " .. (err.message or vim.inspect(err)), vim.log.levels.ERROR)
+      return
+    end
+    vim.notify("Renamed collection to: " .. collection.displayName, vim.log.levels.INFO)
+  end)
+end, { nargs = "+", desc = "Rename collection" })
+
+vim.api.nvim_create_user_command("NeoweaverCollectionDelete", function(opts)
+  local collections = require("neoweaver._internal.collections")
+  local id = tonumber(opts.args)
+  
+  if not id then
+    vim.notify("Usage: :NeoweaverCollectionDelete <id>", vim.log.levels.WARN)
+    return
+  end
+  
+  -- Confirm deletion
+  vim.ui.input({ prompt = "Delete collection " .. id .. "? (y/N): " }, function(confirm)
+    if confirm ~= "y" and confirm ~= "Y" then
+      return
+    end
+    
+    collections.delete_collection(id, function(success, err)
+      if err then
+        vim.notify("Failed to delete collection: " .. (err.message or vim.inspect(err)), vim.log.levels.ERROR)
+        return
+      end
+      vim.notify("Deleted collection", vim.log.levels.INFO)
+    end)
+  end)
+end, { nargs = 1, desc = "Delete collection" })
+
+vim.api.nvim_create_user_command("NeoweaverCollectionUpdate", function(opts)
+  local collections = require("neoweaver._internal.collections")
+  
+  -- Parse args: id [displayName=value] [parentId=value] [description=value]
+  local args = vim.split(opts.args, "%s+", { trimempty = true })
+  local id = tonumber(args[1])
+  
+  if not id then
+    vim.notify("Usage: :NeoweaverCollectionUpdate <id> [displayName=value] [parentId=value]", vim.log.levels.WARN)
+    return
+  end
+  
+  local update_opts = {}
+  for i = 2, #args do
+    local key, value = args[i]:match("^([^=]+)=(.+)$")
+    if key == "displayName" then
+      update_opts.displayName = value
+    elseif key == "parentId" then
+      update_opts.parentId = tonumber(value)
+    elseif key == "description" then
+      update_opts.description = value
+    end
+  end
+  
+  if vim.tbl_isempty(update_opts) then
+    vim.notify("No update options provided", vim.log.levels.WARN)
+    return
+  end
+  
+  collections.update_collection(id, update_opts, function(collection, err)
+    if err then
+      vim.notify("Failed to update collection: " .. (err.message or vim.inspect(err)), vim.log.levels.ERROR)
+      return
+    end
+    vim.notify("Updated collection: " .. collection.displayName, vim.log.levels.INFO)
+  end)
+end, { nargs = "+", desc = "Update collection" })
